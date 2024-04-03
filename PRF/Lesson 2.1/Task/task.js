96cc74ee195aba62af20afebfcbe021f49d2cb66
Askvig <arneaskvig@gmail.com>
2024-04-03 20:22:30 +0200

    progressed with task 2.1

--- PRF/Lesson 2.1/Task/task.js
+++ PRF/Lesson 2.1/Task/task.js
@@ -12,16 +12,18 @@ class Dish {
 }
 
 class Pizza extends Dish {
+    #ingredient;
     constructor(name, price, ingredient) {
         super(name, price);
-        this.ingredient = ingredient;
+        this.#ingredient = ingredient;
     }
 }
 
 class Pancake extends Dish {
+    #sweet;
     constructor(name, price, sweet) {
         super(name, price);
-        this.sweet = sweet;
+        this.#sweet = sweet;
     }
 }
 
@@ -31,3 +33,27 @@ class Pasta extends Dish {
         this.isVegetarian = isVegetarian;
     }
 }
+
+const suppe = new Dish('Tomatsuppe', '99kr');
+const brød = new Dish('Hvetebrød', '19kr');
+const margherita = new Pizza('Margherita', '299kr', ['Ost', 'Oregano']);
+const skinke = new Pizza('Skinke', '299kr', ['Skinke', 'Oregano']);
+const ronnie = new Pancake('Ronnie', '249kr', true);
+const calimero = new Pancake('Calimero', '269kr', true);
+const bolognese = new Pasta('Bolognese', '150kr', false);
+const carbonara = new Pasta('Carbonara', '190kr', false);
+
+console.log(suppe);
+console.log(brød);
+console.log(margherita);
+console.log(skinke);
+console.log(ronnie);
+console.log(ronnie.name);
+console.log(ronnie.price);
+console.log(ronnie.sweet);
+console.log(calimero);
+console.log(bolognese);
+console.log(bolognese.name);
+console.log(bolognese.price);
+console.log(bolognese.isVegetarian);
+console.log(carbonara);
