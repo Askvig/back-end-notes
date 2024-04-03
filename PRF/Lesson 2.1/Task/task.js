0ac45e03f636810e7b71842dc2366f8aadb583cd
Askvig <arneaskvig@gmail.com>
2024-04-03 22:45:19 +0200

    completed task 2.1

--- PRF/Lesson 2.1/Task/task.js
+++ PRF/Lesson 2.1/Task/task.js
@@ -9,6 +9,9 @@ class Dish {
         this.name = name;
         this.price = price;
     }
+    food() {
+        return this.name;
+    }
 }
 
 class Pizza extends Dish {
@@ -17,13 +20,19 @@ class Pizza extends Dish {
         super(name, price);
         this.#ingredient = ingredient;
     }
+    getIngredients() {
+        return this.#ingredient;
+    }
 }
 
 class Pancake extends Dish {
-    #sweet;
+    #sweet
     constructor(name, price, sweet) {
         super(name, price);
         this.#sweet = sweet;
+    }
+    isSweet () {
+        return this.#sweet;
     }
 }
 
@@ -43,17 +52,31 @@ const calimero = new Pancake('Calimero', '269kr', true);
 const bolognese = new Pasta('Bolognese', '150kr', false);
 const carbonara = new Pasta('Carbonara', '190kr', false);
 
-console.log(suppe);
-console.log(brød);
-console.log(margherita);
-console.log(skinke);
-console.log(ronnie);
-console.log(ronnie.name);
-console.log(ronnie.price);
-console.log(ronnie.sweet);
-console.log(calimero);
-console.log(bolognese);
-console.log(bolognese.name);
-console.log(bolognese.price);
-console.log(bolognese.isVegetarian);
-console.log(carbonara);
+
+console.log(suppe); // Dish { name: 'Tomatsuppe', price: '99kr' }
+console.log(brød); // Dish { name: 'Hvetebrød', price: '19kr' }
+
+console.log(margherita); // Pizza { name: 'Margherita', price: '299kr' }
+console.log(margherita.food()); // Margherita
+margherita.name = "hamburger";
+// console.log(margherita.getDish()); // Margherita
+console.log(margherita.food()); // hamburger
+
+console.log(margherita.price); // 299kr
+console.log(margherita.ingredient); // undefined
+console.log(margherita.getIngredients()); // [ 'Ost', 'Oregano' ]
+
+console.log(skinke); // Pizza { name: 'Skinke', price: '299kr' }
+console.log(ronnie); // Pancake { name: 'Ronnie', price: '249kr' }
+console.log(ronnie.name); // Ronnie
+console.log(ronnie.price); // 249kr
+console.log(ronnie.sweet); // undefined
+console.log(ronnie.isSweet()); // true
+console.log(calimero); // Pancake { name: 'Calimero', price: '269kr' }
+console.log(bolognese); // Pasta { name: 'Bolognese', price: '150kr', isVegetarian: false }
+console.log(bolognese.name); // Bolognese
+console.log(bolognese.price); // 150kr
+console.log(bolognese.isVegetarian); // false
+console.log(carbonara); // Pasta { name: 'Carbonara', price: '190kr', isVegetarian: false }
+
+console.log(ronnie instanceof Pancake); // true
