5e52eccc5140aa6c3502c8370bfc9c20a540b960
Askvig <arneaskvig@gmail.com>
2024-04-14 04:46:04 +0200

    tasks from self-study

---
+++ PRF/Lesson 2.5 Self-study/Task/task.js
@@ -0,0 +1,82 @@
+// Create a ‘Hello’ function, that returns ‘Hello world’.
+function Hello() {
+    return 'Hello world';
+}
+
+// Define two functions. The first function a should return ‘Hello a!’ and the second function should return ‘Hello b!’
+function HelloA() {
+    return 'Hello a!';
+}
+
+function HelloB() {
+    return 'Hello b!';
+}
+
+// Write a function ‘Greet’ that takes one parameter and returns the 'Hello <parameter>!'
+function Greet(parameter) {
+    return `Hello ${parameter}!`;
+}
+
+// Write a function ‘Log’ that takes a parameter and logs it.
+function Log(parameter) {
+    console.log(parameter)
+}
+
+// Write a function ‘MyStringFunction’ that takes a string and returns the number of characters in that string. (Make use of string.length).
+function MyStringFunction(string) {
+    return string.length;
+}
+
+// Write a function ‘Upper’ that takes a string and returns the uppercase of the string.
+function Upper(string) {
+    return string.toUpperCase();
+}
+
+// Write a function ‘Add’ that takes two numbers and returns their sum.
+function Add(num1, num2) {
+    return num1 + num2;
+}
+const add = (num1, num2) => num1 + num2;
+
+// Write a function ‘Modulo’ that takes a natural number and a dividend and returns the remainder of the modulo (%) of the two numbers.
+function Modulo(num1, num2) {
+    return num1 % num2;
+}
+
+// Write a function ‘MinMax’ that finds the min and max numbers of an array of numbers.
+function MinMax(array) {
+
+    
+}
+
+// Write a function ‘Parse’ that takes a string (‘20’) and a number (5) and returns the multiplication of the string and the number (use the parseInt function to read the string as a number).
+
+
+// Write a function that accepts two numbers and checks if the numbers are equal. After the condition is checked, log an appropriate message to the console.
+
+
+// Write a function ‘toArray’ that accepts four values and returns an array of these values.
+
+
+// Write a function ‘ArrayLength’ that takes an array of values and returns the array’s length.
+
+
+// Write a function ‘ArraySort’ that takes an array of values and returns a sorted array. (Make use of the sort() function).
+
+
+// Create a js file containing a line comment and a multiline comment/block comment.
+
+
+// Write a function ‘MyForLoop’ that loops until a variable equals 10. The variable should be incremented by ‘1’ in each iteration. (Make use of the for-loop).
+
+
+// Write a function ‘MyWhileLoop’ that declares a variable set to ‘10’ and loops until that variable is equal to ‘0’. The variable should be decremented by ‘1’ in each iteration. (Make use of the while-loop).
+
+
+// Write a function that checks if a given value is a number. (Make use of the isNan() function). Return a Boolean of the result.
+
+
+// Write a function ‘SplitFunction’ that accepts the string ‘7+12+100’ and splits it into individual values, then summing these values. (Make use of the split() and parseInt() functions). Return the summed result.
+
+
+// Write a function ‘Factorial’ that accepts a positive integer and calculates the factorial of that integer. Return the result.
